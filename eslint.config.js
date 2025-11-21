03683ae983a88893091392b81cc337b03c14a707
jiazhigang <jiazhigang_888@163.com>
2025-11-21 09:47:50 +0800

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,53 @@
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginVue from 'eslint-plugin-vue'
+import prettier from 'eslint-plugin-prettier/recommended'
+import autoImportGlobals from './.eslintrc-auto-import.json' with { type: 'json' }
+
+export default [
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/recommended'],
+  prettier,
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+      },
+      globals: {
+        ...autoImportGlobals.globals,
+        window: 'readonly',
+        document: 'readonly',
+        navigator: 'readonly',
+        requestAnimationFrame: 'readonly',
+        cancelAnimationFrame: 'readonly',
+        HTMLDivElement: 'readonly'
+      }
+    }
+  },
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_'
+        }
+      ],
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto'
+        }
+      ]
+    }
+  },
+  {
+    ignores: ['dist', 'node_modules', '*.config.js']
+  }
+]
