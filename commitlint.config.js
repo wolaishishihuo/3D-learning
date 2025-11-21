7988a1702dcdb796c2189092fc70d133ab8405f9
wolaishishihuo <2430250298@qq.com>
2025-11-21 11:04:30 +0800

    ✨ feat: refactor Home and BasicScene views, add Beams component, and update routing structure

--- commitlint.config.js
+++ commitlint.config.js
@@ -19,6 +19,7 @@ export default {
       ]
     ],
     'subject-case': [0], // subject大小写不做校验
+    'header-max-length': [2, 'always', 200], // 增加 header 最大长度到 200
     // 允许以 emoji 开头
     'subject-empty': [2, 'never'],
     'type-empty': [2, 'never']
